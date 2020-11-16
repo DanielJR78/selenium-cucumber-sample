23e94c2f4c69ab793bc0d18428d2eb6a4428594b
DanielJR78 <50712582+DanielJR78@users.noreply.github.com>
2020-11-16 13:24:45 +0100

    Create stepdefs.js

---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,14 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const assert = require('assert');
+
+Given('today is Sunday', () => {
+  //
+});
+
+When('I ask whether its Friday yet', () => {
+  //
+});
+
+Then('I should be told', () => {
+  //
+});
